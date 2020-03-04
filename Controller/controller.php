cb514225bf559e6403569f7a5d737022d6c17885
Hausmann <william.hausmann@cpnv.ch>
2020-03-04 15:00:16 +0100

    Push de la template MVC
    Push d'une template HTML/CSS adaptée à un MVC PHP

---
+++ Controller/controller.php
@@ -0,0 +1,11 @@
+<?php
+
+require "modele/modele.php";
+
+// Affichage de la page de l'accueil
+function accueil()
+{
+    require "vue/vue_accueil.php";
+}
+
+?>
--- index.php
+++ index.php
@@ -1,5 +1,39 @@
 <?php
 
-phpinfo();
+session_start();
+require "Controller/controller.php";
+/*if (@$_GET['logout']) {
+    session_destroy();
+    header("location:index.php");
+}*/
 
-?>
+try {
+    //pour passer d'une page a une autre
+    if (isset($_GET['action'])) {
+        $action = $_GET['action'];
+        switch ($action) {
+            //page d'accueil
+            case 'vue_accueil' :
+                accueil();
+                break;
+            //page pour visualiser toutes les observations
+            case 'vue_observation' :
+                observation();
+                break;
+            //pour se login
+            case 'vue_login' :
+                login(@$_POST['login'], @$_POST['password']);
+                break;
+            //pour s'inscrire
+            case 'vue_inscription' :
+                inscription(@$_POST['prenomForm'], @$_POST['nomForm'], @$_POST['usernameForm'], @$_POST['emailForm'], @$_POST['confEmailForm'], @$_POST['passwordForm'], @$_POST['confPasswordForm']);
+                break;
+            default :
+                throw new Exception("Action non valide");
+        }
+    } else
+        //aller sur la page d'accueil
+        accueil();
+} catch (Exception $e) {
+    erreur($e->getMessage());
+}
